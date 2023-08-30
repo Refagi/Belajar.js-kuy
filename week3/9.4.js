bc486430a4dd8a3f515d36fd56793e9433d93900
Refagi <140000551+Refagi@users.noreply.github.com>
2023-08-30 14:21:01 +0700

    Update 9.4.js

--- week3/9.4.js
+++ week3/9.4.js
@@ -87,24 +87,24 @@ console.log(urutkanAbjad("aegis")); // 'aegis'
 
 
 //TIPS: gunakan method toUpperCase() dan toLowerCase()
-// function tukarBesarKecil(kalimat) {
-//   let result = "";// buat variable kosong untuk menampung hasil
-//   for (let i = 0; i < kalimat.length; i++){
-//     if (kalimat[i] === kalimat[i].toUpperCase()){
-//       result += kalimat[i].toLowerCase()
-//     } else {
-//       result += kalimat[i].toUpperCase()
-//     }
-//   }
-//   return result
-// }
+function tukarBesarKecil(kalimat) {
+  let result = "";// buat variable kosong untuk menampung hasil
+  for (let i = 0; i < kalimat.length; i++){
+    if (kalimat[i] === kalimat[i].toUpperCase()){
+      result += kalimat[i].toLowerCase()
+    } else {
+      result += kalimat[i].toUpperCase()
+    }
+  }
+  return result
+}
 
-// // TEST CASES
-// console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
-// console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
-// console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-// console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
-// console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+// TEST CASES
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
 
 
 
