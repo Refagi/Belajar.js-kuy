39a4521c79f909377201b5413987fb2891d50328
Refagi <renogivaro@gmail.com>
2023-11-09 09:15:00 +0700

    materi baru

--- phase0/week1/part3.js
+++ phase0/week1/part3.js
@@ -62,3 +62,73 @@ console.log(true + 20);
 // let bisa diubah sedangkan const tidak bisa
 // komentar menggunakan (ctrl + /)
 
+//mengubah bilangan biner, hexadesimal dan oktal kedalam bilangan desimal
+let biner = 0b0110110110;
+console.log(biner)//438
+
+let hexadesimal = 0xBC
+console.log(hexadesimal)//188
+
+let oktal = 0o176
+console.log(oktal)//126
+
+
+//escape sequence
+let enter = "reno\nfaza\ngivaro"
+console.log(enter) 
+/*reno
+faza
+givaro */
+
+let tab = "reno\tfaza\tgivaro"
+console.log(tab)//reno    faza    givaro
+
+let petikSatu = "reno\'faza\'givaro"
+console.log(petikSatu)//reno'faza'givaro
+
+let petikDua = "reno\"faza\"givarao"
+console.log(petikDua)//reno"faza"givarao
+
+let garisMiring = "reno\\faza\\givaro"
+console.log(garisMiring)//reno\faza\givaro 
+
+
+/*
+console method
+console.info atau console.log, untuk memberi tahy informasi
+console.warm, untuk memberi tahu informasi peringatan
+console.error, untuk memberi tahu informasi error
+console.table, untuk memberi informasi dalam bentuk table */
+
+
+//ternary operator
+let count = 70
+let test = count >= 90 ? "bener":"salah"
+console.log(test)//salah
+
+
+//nullist method
+let cek;
+let cek2 = cek
+if (cek === null || cek === undefined)
+  cek2 = "waduh"
+console.log(cek2)
+
+let cekCok;
+let cek3 = cekCok ?? "waduh gaes"
+console.log(cek3)
+
+
+
+let arr = [5,'']
+let arrCek = arr[0] || arr[1];
+console.info(arrCek)//5
+/*ini mencari nilai yang true karena '' adalah false maka yg diambil adalah 5*/
+
+let obj = {
+    nama1 : 'reno',
+    nama2 : `''`
+}
+let cekObj = obj.nama1 && obj.nama2
+console.log(cekObj)//''
+/*ini kebalikannya mencari nilai yang false */
