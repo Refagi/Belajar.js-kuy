9472370f4874883c888ff22827cb14b51adcc212
Refagi <renogivaro@gmail.com>
2023-11-16 13:04:28 +0700

    push satu soal

--- phase0/latihan/codeWars1.js
+++ phase0/latihan/codeWars1.js
@@ -450,3 +450,20 @@ console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk',
 console.log(findNeedle( [1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]))//found the needle at position 30
 
 
+
+function squareOrSquareRoot(array) {
+  //jika array memiliki akar kuadrat maka tampilkan akar kuadratnya, jika tidak pangkatkan 2
+  let result = []
+ for (let i = 0; i < array.length; i++){
+  if (array[i] % Math.sqrt(array[i]) !== 0){
+      result.push(Math.pow(array[i], 2))
+  } else {
+      result.push(Math.sqrt(array[i]))
+  }
+ }
+  return result;  
+}
+console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]))//[2,9,3,49,4,1]
+console.log(squareOrSquareRoot([ 2, 9, 3, 49, 4, 1 ]))//[ 4, 3, 9, 7, 2, 1 ]
+
+
