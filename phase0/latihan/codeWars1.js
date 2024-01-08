172f8f78558fee00ee37317109e5318c80695c78
Refagi <renogivaro@gmail.com>
2024-01-08 14:57:37 +0700

    tambah latihan dari pascal

--- phase0/latihan/codeWars1.js
+++ phase0/latihan/codeWars1.js
@@ -467,3 +467,23 @@ console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]))//[2,9,3,49,4,1]
 console.log(squareOrSquareRoot([ 2, 9, 3, 49, 4, 1 ]))//[ 4, 3, 9, 7, 2, 1 ]
 
 
+function xo (baris, kolom){
+
+  for (let i = 1; i <= baris; i++){
+      let result = ''
+      for (let j = 1; j <= kolom; j++ ){
+          if ((i + j) % 2 !== 0){
+              result += 'O' + ' '
+          } else {
+              result += 'X' + ' '
+          }
+      }
+      console.log(result)
+  }
+
+}
+
+console.log(xo(3, 5))
+// X O X O X 
+// O X O X O
+// X O X O X
