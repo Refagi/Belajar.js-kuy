6ef1a855059b4df3405dac145ed3830394507ebc
Refagi <renogivaro@gmail.com>
2024-03-11 20:50:39 +0700

    add materi prisma

---
+++ phase1/week4/part30-prisma.js
@@ -0,0 +1,35 @@
+/**Prisma adalah sebuah alat (tool) yang digunakan dalam pengembangan aplikasi web untuk mengelola basis data 
+ * dan melakukan operasi database dengan lebih mudah dan aman. Prisma membantu pengembang dalam membangun 
+ * aplikasi dengan menggunakan bahasa pemrograman JavaScript dan TypeScript.
+
+Berikut beberapa fitur utama dan konsep yang terkait dengan Prisma:
+
+1.ORM (Object-Relational Mapping): Prisma adalah ORM yang memungkinkan pengembang untuk berinteraksi dengan basis 
+data relasional seperti PostgreSQL, MySQL, SQLite, dan SQL Server menggunakan kode JavaScript atau TypeScript. 
+Ini menghilangkan kebutuhan untuk menulis kueri SQL secara manual.
+
+2.Pengelolaan Skema Database: Prisma memungkinkan pengembang untuk mendefinisikan struktur database dengan 
+menggunakan sintaks yang mudah dipahami. Struktur ini disebut "Prisma Schema" dan digunakan untuk menghasilkan 
+skema basis data secara otomatis.
+
+3.Migrations: Prisma menyediakan alat untuk mengelola dan menerapkan perubahan struktur basis data dengan aman 
+melalui proses yang disebut "migrasi". Ini memungkinkan perubahan skema tanpa kehilangan data yang ada.
+
+4.Query yang Tipe Aman: Prisma menggunakan tipe statis (static typing) dari TypeScript untuk memastikan kueri ke 
+basis data memiliki tipe data yang benar. Ini membantu menghindari banyak kesalahan yang sering terjadi dalam 
+pengembangan basis data.
+
+5.Query yang Mudah Digunakan: Prisma menyediakan API yang intuitif dan mudah digunakan untuk melakukan berbagai 
+operasi database seperti penambahan, pembacaan, pembaruan, dan penghapusan data.
+
+6.Pengelolaan Koneksi Database: Prisma mengelola koneksi ke basis data dengan baik, termasuk fitur seperti 
+pooling koneksi untuk meningkatkan efisiensi.
+
+7.Keamanan: Prisma menyertakan keamanan bawaan seperti penanganan SQL injection yang aman.
+
+Prisma memungkinkan pengembang untuk fokus pada logika bisnis aplikasi mereka daripada menghabiskan banyak waktu 
+untuk menulis kueri database yang rumit. Ini memudahkan pengembangan aplikasi web yang skala besar dan kompleks 
+dengan meminimalkan kompleksitas operasi database. Prisma juga berintegrasi dengan berbagai framework dan teknologi 
+populer dalam ekosistem JavaScript, membuatnya menjadi pilihan yang kuat untuk pengembangan aplikasi modern.
+
+Explore : https://www.prisma.io/ */
