19349c6a2dd558f4e925368d1dddf03a0300ebdd
Refagi <renogivaro@gmail.com>
2024-06-16 16:43:38 +0700

    push code wars

--- phase0/latihan/index.js
+++ phase0/latihan/index.js
@@ -487,3 +487,16 @@ console.log(xo(3, 5))
 // X O X O X 
 // O X O X O
 // X O X O X
+
+function solution(a, b){
+  if(a.length > b.length){
+    return `${b}${a}${b}`
+  } else {
+    return `${a}${b}${a}`
+  }
+}
+
+console.log(solution('45', '1'));
+console.log(solution('13', '200'));
+console.log(solution('Soon', 'Me'));
+console.log(solution('U', 'False'));
